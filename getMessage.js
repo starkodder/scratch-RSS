0289b642b662f303c5284f58158e1e70d0dbc569
starkodder <74370377+starkodder@users.noreply.github.com>
2021-05-11 17:37:41 +0200

    Update getMessage.js

--- getMessage.js
+++ getMessage.js
@@ -1,4 +1,5 @@
+var url = window.location.pathname.split("/");
 var id = url[url.length-2];
-        window.fetch('https://api.scratch.mit.edu/projects/' + id)
-        .then(data => data.json())
-        .then(data => alert(data.history.modified));
+window.fetch('https://api.scratch.mit.edu/projects/' + id)
+  .then(data => data.json())
+  .then(data => alert(data.history.modified));
